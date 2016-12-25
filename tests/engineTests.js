ba2839297ab99c7909e0c90350e260257f2ace33
Alex Booker <alexbooker@fastmail.im>
2016-12-25 02:06:13 +0000

    Test that player can be added to a race

--- tests/engineTests.js
+++ tests/engineTests.js
@@ -7,3 +7,15 @@ test('race doesn\'t start', t => {
   const race = engine.createRace()
   t.false(race.started)
 })
+
+test('addPlayer', t => {
+  const sut = engine.createRace()
+  const expectedName = 'foo'
+  const expectedId = sut.addPlayer(expectedName)
+  const {
+    name: actualName,
+    uuid: actualId
+  } = sut.players[0]
+  t.true(actualName === expectedName)
+  t.true(actualId === expectedId)
+})
